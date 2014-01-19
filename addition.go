b82057f8a426c7cc7b0922be625f82ad57ed36a1
Charles <iamreiver@gmail.com>
2014-01-18 22:07:01 -0800

    added code for addition operation.

---
+++ addition.go
@@ -0,0 +1,23 @@
+package float64x4
+
+
+
+func (a T) Add(b T) T {
+
+	return T{
+		X: a.X + b.X,
+		Y: a.Y + b.Y,
+		Z: a.Z + b.Z,
+		W: a.W + b.W,
+	}
+}
+
+
+
+func (a *T) AddTo(b T) {
+
+	a.X += b.X
+	a.Y += b.Y
+	a.Z += b.Z
+	a.W += b.W
+}
