613069ebeab4d581dba33947d70fe259564a0ba2
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-01-04 01:26:18 -0800

    added docs

--- float64x4.go
+++ float64x4.go
@@ -1,19 +1,28 @@
 package float64x4
 
+// T represents the floating point approximation of ℝ⁴.
+// I.e., 4 float64.
+//
+// Note that these can be used these can be used for
+// affine transformation in ℝ³.
 type T [4]float64
 
+// X returns the 1st dimension.
 func (receiver T) X() float64 {
 	return receiver[x]
 }
 
+// Y returns the 2nd dimension.
 func (receiver T) Y() float64 {
 	return receiver[y]
 }
 
+// Z returns the 3rd dimension.
 func (receiver T) Z() float64 {
 	return receiver[z]
 }
 
+// W returns the 4th dimension.
 func (receiver T) W() float64 {
 	return receiver[w]
 }
